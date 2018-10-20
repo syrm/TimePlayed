876fa00cffdaa99fbc3fb40485bc4f7df52f297c
Valentijn <33581307+iGotYourBack@users.noreply.github.com>
2018-10-20 09:31:35 +0200

    Remove unused select query

--- commands/setPrivacy.js
+++ commands/setPrivacy.js
@@ -1,22 +1,20 @@
 var tools = require("../tools")
-var connection = tools.getConnection
+var connection = tools.getConnection()
 module.exports = function(obj) {
   var message = obj.message;
   var lang = obj.lang;
   message.channel.send(lang.commands.setPrivacy.choices).then(msg => {
     tools.awaitReaction(msg, ["🇦", "🇧"], message.author.id, function(reaction, choice) {
-      connection.query(`SELECT * FROM privateUsers WHERE userID=?`, [message.author.id], function(error, results, fields) {
-        if(choice == 1) {
-          connection.query(`DELETE FROM privateUsers WHERE userID=?`, [message.author.id], function(error, results, fields) {
-            return message.reply(lang.commands.setPrivacy.setPublic)
-          })
-        }
-        if(choice == 0) {
-          connection.query("INSERT IGNORE INTO privateUsers (userID) VALUES (?)", [message.author.id], function(error, results, fields) {
-            return message.reply(lang.commands.setPrivacy.setPrivate)
-          })
-        }
-      })
+      if(choice == 1) {
+        connection.query(`DELETE FROM privateUsers WHERE userID=?`, [message.author.id], function(error, results, fields) {
+          return message.reply(lang.commands.setPrivacy.setPublic)
+        })
+      }
+      if(choice == 0) {
+        connection.query("INSERT IGNORE INTO privateUsers (userID) VALUES (?)", [message.author.id], function(error, results, fields) {
+          return message.reply(lang.commands.setPrivacy.setPrivate)
+        })
+      }
     })
   })
 }
