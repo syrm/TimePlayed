17a7d51a0d4bc1743c2cf8838f2e3d972790b9eb
Valentijn <33581307+iGotYourBack@users.noreply.github.com>
2018-11-03 09:59:34 +0100

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -3,14 +3,24 @@ var connection = tools.getConnection
 module.exports = function(obj) {
     var message = obj.message;
     var lang = obj.lang;
-    connection.query("SELECT count(*) FROM afk WHERE userID=?", [message.author.id], function(error, results, fields) {
-        if(results[0]["count(*)"] > 0) {
+    connection.query("SELECT * FROM afk WHERE userID=?", [message.author.id], function(error, results, fields) {
+        if(results.length > 0) {
+            // If user is AFK
             connection.query("DELETE FROM afk WHERE userID=?", [message.author.id], function(error, results, fields) {
-                return message.reply(lang.commands.afk.toggleOff)
+                if(message.author.presence.game) {
+                    connection.query("INSERT INTO playtime (userID, game, startDate) VALUES (?, ?, ?)", [message.author.id, message.author.presence.game.name, new Date()], function(error, results, fields) {
+                        return message.reply(lang.commands.afk.toggleOff)
+                    })
+                } else {
+                    return message.reply(lang.commands.afk.toggleOff)
+                }
             })
         } else {
-            connection.query(`UPDATE playtime SET endDate=? WHERE userID=? AND endDate IS NULL`, [new Date(), message.author.id], function(error, results, fields) {
-                return message.reply(lang.commands.afk.toggleOn);
+            // If user is not AFK
+            connection.query("INSERT INTO afk (userID) VALUES (?)", [message.author.id], function(error, results, fields) {
+                connection.query(`UPDATE playtime SET endDate=? WHERE userID=? AND endDate IS NULL`, [new Date(), message.author.id], function(error, results, fields) {
+                    return message.reply(lang.commands.afk.toggleOn);
+                })
             })
         }
     })
