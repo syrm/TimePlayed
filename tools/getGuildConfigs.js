00b809ac44e30d4dfeea4aaf143f21466ce36b57
Valentijn <33581307+iGotYourBack@users.noreply.github.com>
2018-11-25 14:57:09 +0100

    Fix crash

--- tools/getGuildConfigs.js
+++ tools/getGuildConfigs.js
@@ -2,8 +2,6 @@ var connection = require('./connection.js');
 var fs = require("fs");
 
 var defaultValues = {
-  prefix: "!!",
-  defaultGame: "Fortnite",
   leaderboardLayout: fs.readFileSync("./tools/assets/default-leaderboard.txt"),
   leaderboardNoMoreToday: "No more users have played %game% today!",
   leaderboardNoMoreWeek: "No more users have played %game% this week!",
@@ -14,26 +12,19 @@ var defaultValues = {
 }
 
 function toConfig(result) {
-  Object.keys(defaultValues).forEach(key => {
-    if(!result[key]) {
-      result[key] = defaultValues[key];
-    }
-  })
-  enableRankingMentions = false;
-  if(result.enableRankingMentions == 1) enableRankingMentions = true;
   var config = {
     prefix: result.prefix,
     rankingChannel: result.rankingChannel,
     defaultGame: result.defaultGame,
-    leaderboardLayout: result.leaderboardLayout,
+    leaderboardLayout: result.leaderboardLayout || defaultValues.leaderboardLayout,
     
-    leaderboardNoMoreToday: result.leaderboardNoMoreToday,
-    leaderboardNoMoreWeek: result.leaderboardNoMoreWeek,
-    leaderboardNoMoreAlways: result.leaderboardNoMoreAlways,
+    leaderboardNoMoreToday: result.leaderboardNoMoreToday || defaultValues.leaderboardNoMoreToday,
+    leaderboardNoMoreWeek: result.leaderboardNoMoreWeek || defaultValues.leaderboardNoMoreWeek,
+    leaderboardNoMoreAlways: result.leaderboardNoMoreAlways || defaultValues.leaderboardNoMoreAlways,
 
-    leaderboardNoToday: result.leaderboardNoToday,
-    leaderboardNoWeek: result.leaderboardNoWeek,
-    leaderboardNoAlways: result.leaderboardNoAlways
+    leaderboardNoToday: result.leaderboardNoToday || defaultValues.leaderboardNoToday,
+    leaderboardNoWeek: result.leaderboardNoWeek || defaultValues.leaderboardNoWeek,
+    leaderboardNoAlways: result.leaderboardNoAlways || defaultValues.leaderboardNoAlways
   }
   return config;
 }
