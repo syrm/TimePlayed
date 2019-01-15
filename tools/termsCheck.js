4990bf9eb3fa88b650e187d93b264685240b4772
xVaql <valentijnvanwinden@gmail.com>
2019-01-15 19:48:40 +0100

    Fix bug

--- tools/termsCheck.js
+++ tools/termsCheck.js
@@ -1,23 +1,18 @@
 var connection = require('./connection.js');
-function convertBinary(bin) {
-  if(bin == 1) return true;
-  if(bin == 0) return false;
-}
 module.exports =  function(executerID, mentionID, guildID, callback) {
   var mentionStatement = ``
-  if(mentionID) mentionStatement = ` OR userID=${mentionID}`
-  connection.query(`SELECT * FROM termsAccept WHERE userID=${executerID}${mentionStatement}`, function(error, results, fields) {
+  if(mentionID) mentionStatement = ` OR userID=?`
+  connection.query(`SELECT * FROM termsAccept WHERE userID=?${mentionStatement}`, [executerID, mentionID], function(error, results, fields) {
     if(error) throw error;
     var accepts = {}
     results.forEach(result => {
-      if(result.userID == executerID) accepts.executer = convertBinary(result.accept)
-      if(result.userID == mentionID) accepts.mention = convertBinary(result.accept)
+      if(result.userID == executerID) accepts.executer = true;
+      if(result.userID == mentionID) accepts.mention = true;
     })
     connection.query("SELECT * FROM premium WHERE guildID=?", [guildID], function(error, results, fields) {
       var premium = false;
       if(results.length > 0) premium = true;
       return callback([accepts, premium])
     })
-    
   })
 }
