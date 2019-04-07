625a7f62f8dd76b6f75b760d076ea0336860eaf5
xVaql <valentijnvanwinden@gmail.com>
2019-04-07 17:40:42 +0200

    Add database failure catch

--- tools/getGuildConfig.js
+++ tools/getGuildConfig.js
@@ -21,6 +21,7 @@ function toConfig(result) {
 
 module.exports = function(guildID, callback) {
   connection.query("SELECT prefix, rankingChannel, defaultGame FROM guildSettings WHERE guildID=?", [guildID], function(error, results, fields) {
+    if(!results) return undefined;
     if(results.length < 1) {
       connection.query("INSERT INTO guildSettings (guildID, prefix, defaultGame) VALUES (?, '!!', 'Fortnite')", [guildID], function(error, results, fields) {
         tools.getGuildConfig(guildID, function(config) {
