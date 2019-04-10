5280fd7a612619b3654b74854f1ff9e5be4b1733
xVaql <valentijnvanwinden@gmail.com>
2019-04-10 16:34:53 +0200

    Temporary logging

--- logger.js
+++ logger.js
@@ -177,17 +177,17 @@ function refreshGuildStats() {
         // If still playing a game
         if(oldMember.presence.game) {
           // If game changed
-          console.log(`GS: ${oldMember.displayName} changed game (from ${oldMember.presence.game.name} to ${newMember.presence.game.name} in ${guildID})`)
+          console.log(`GS: ${oldMember.displayName} changed game (from ${oldMember.presence.game.name} to ${newMember.presence.game.name} in ${guildID} user ${oldMember.id})`)
           toEnd.push(oldMember.id)
           toInsert.push([oldMember.id, guildID, date, newMember.presence.game.name])
         } else {
-          // If started playing 
-          console.log(`GS: ${oldMember.displayName} started playing ${newMember.presence.game.name} (in ${guildID})`)
+          // If started playing
+          console.log(`GS: ${oldMember.displayName} started playing ${newMember.presence.game.name} (in ${guildID} user ${oldMember.id})`)
           toInsert.push([oldMember.id, guildID, date, newMember.presence.game.name])
         }
       } else if(oldMember.presence.game) {
         // If stopped playing
-        console.log(`GS: ${oldMember.displayName} stopped playing ${oldMember.presence.game.name} (in ${guildID})`)
+        console.log(`GS: ${oldMember.displayName} stopped playing ${oldMember.presence.game.name} (in ${guildID} user ${oldMember.id})`)
         toEnd.push(oldMember.id)
       }
     })
